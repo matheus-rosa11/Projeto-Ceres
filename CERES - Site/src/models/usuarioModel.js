ff84e9d9af5ed96bcd6f28bd1f156c4af3cf749c
Gdmasceno <gabriel.doliveira@sptech.school>
2022-05-18 22:14:02 -0300

    adicionando o cadastro do tipo adm

--- CERES - Site/src/models/usuarioModel.js
+++ CERES - Site/src/models/usuarioModel.js
@@ -49,9 +49,24 @@ function cadastrar(nome, email, cnpj, senha) {
     return database.executar(instrucao);
 }
 
+
+function cadastrarAdm(nome, email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email,senha);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+
+    var instrucao = `
+    INSERT INTO usuario (email, senha, tipo, fkEmpresa) VALUES ('${email}','${senha}','admin',(SELECT idEmpresa FROM empresa WHERE cnpj = 1));
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
 module.exports = {
     entrar,
     entrarAdm,
     cadastrar,
+    cadastrarAdm,
     listar,
 };
