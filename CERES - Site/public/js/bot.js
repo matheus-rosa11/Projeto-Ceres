8042c0cdbf9102a2fcd4b5f50ef8af1ff4be7a54
Gdmasceno <gabriel.doliveira@sptech.school>
2022-05-22 11:39:50 -0300

    Terminando a tomada de decisão do bot

--- CERES - Site/public/js/bot.js
+++ CERES - Site/public/js/bot.js
@@ -1,4 +1,4 @@
-function selecao3() {
+function outro() {
     // msg1.style.display = "none";
     msg2.style.display = "block";
     descMsg.innerHTML = "Estou com outro problema/dúvida";
@@ -12,5 +12,67 @@ function selecao3() {
 }
 
 function irForm(){
+    fecharBot();
     window.location = "../suporte.html";
+}
+
+function conta(){
+    msg2.style.display = "block";
+    descMsg.innerHTML = "Estou tendo problemas com a conta";
+    msg3.style.display = "block";
+    descMsg2.innerHTML = "Por favor, baixe um PDF com a solução dos principais problemas <a href='../../resources/img/bar-chart.png' download>clicando aqui</a>, e caso seu problema não tenha sido resolvido clique no botão abaixo 'Abrir chamado'";
+    botMain.scrollTo(0,1000);
+
+    select1.style.display = "none";
+    select2.style.display = "none";
+    select3.style.display = "none";
+
+    select7.style.display = "block";
+}
+
+function produto(){
+    msg2.style.display = "block";
+    descMsg.innerHTML = "Estou tendo problemas com o produto";
+    msg3.style.display = "block";
+    descMsg2.innerHTML = "O seu problema é referente ao produto (plataforma) ou produto (sensores)?";
+    botMain.scrollTo(0,1000);
+
+    select1.style.display = "none";
+    select2.style.display = "none";
+    select3.style.display = "none";
+
+    select5.style.display = "block";
+    select6.style.display = "block";
+}
+
+function plataforma(){
+    msg4.style.display = "block";
+    descMsg4.innerHTML = "Com o produto (plataforma)";
+    msg5.style.display = "block";
+    descMsg5.innerHTML = "Por favor, baixe um PDF com a solução dos principais problemas <a href='../../resources/img/bar-chart.png' download>clicando aqui</a>, e caso seu problema não tenha sido resolvido clique no botão abaixo 'Abrir chamado' ";
+    botMain.scrollTo(0,1000);
+
+    select5.style.display = "none";
+    select6.style.display = "none";
+
+    select7.style.display = "block";
+}
+
+function sensores(){
+    msg4.style.display = "block";
+    descMsg4.innerHTML = "Com o produto (sensores)";
+    msg5.style.display = "block";
+    descMsg5.innerHTML = "Por favor, baixe um PDF com a solução dos principais problemas <a href='../../resources/img/bar-chart.png' download>clicando aqui</a>, e caso seu problema não tenha sido resolvido clique no botão abaixo 'Abrir chamado' ";
+    botMain.scrollTo(0,1000);
+
+    select5.style.display = "none";
+    select6.style.display = "none";
+
+    select7.style.display = "block";
+}
+
+
+function fecharBot(){
+    bot.style.display = "none";
+    window.location.reload();
 }
