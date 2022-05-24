b9a64b535c5104ca608a4dd7cc5d78084d6494ba
Gdmasceno <gabriel.doliveira@sptech.school>
2022-05-24 20:44:11 -0300

    adicionando a primeira parte do gráfico funcionando localmente

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    user: "aluno",
+    user: "root",
     database: "ceres",
-    password: "sptech",
+    password: "Kkmjpkjb2021",
 };
 
 function executar(instrucao) {
--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -1,55 +1,51 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
-    
+function buscarUltimasMedidas(idPlantacao, limite_linhas) {
+
     instrucaoSql = ''
-    
+
     if (process.env.AMBIENTE_PROCESSO == "producao") {
         instrucaoSql = `select top ${limite_linhas}
-                        temperatura, 
-                        umidade, 
-                        momento,
-                        CONVERT(varchar, momento, 108) as momento_grafico
-                    from medida
-                    where fk_aquario = ${idAquario}
-                    order by id desc`;
+        umidade, dtCaptura,
+                        CONVERT(varchar, dtCaptura, 108) as momento_grafico
+                    from dadosdht11 join sensor on fkSensor = idSensor
+                    where sensor.fkPlantacao = ${idPlantacao}
+                    order by sensor.fkPlantacao desc`;
+
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-                        temperatura, 
-                        umidade, 
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    from medida
-                    where fk_aquario = ${idAquario}
-                    order by id desc limit ${limite_linhas}`;
+        instrucaoSql = `select umidade, dtCaptura from dadosdht11 join sensor on fkSensor = idSensor
+        where sensor.fkPlantacao = ${idPlantacao} order by sensor.fkPlantacao desc limit ${limite_linhas};`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
     }
-    
+
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal(idAquario) {
-    
+function buscarMedidasEmTempoReal(idPlantacao) {
+
     instrucaoSql = ''
-    
-    if (process.env.AMBIENTE_PROCESSO == "producao") {       
+
+    if (process.env.AMBIENTE_PROCESSO == "producao") {
         instrucaoSql = `select top 1
-                        temperatura, 
-                        umidade, CONVERT(varchar, momento, 108) as momento_grafico, 
+        dht11_temperatura as temperatura, 
+        dht11_umidade as umidade,  
+                        CONVERT(varchar, momento, 108) as momento_grafico, 
                         fk_aquario 
                         from medida where fk_aquario = ${idAquario} 
                     order by id desc`;
-        
+
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `select 
-                        temperatura, 
-                        umidade, DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc limit 1`;
+        umidade, temperatura,
+                        DATE_FORMAT(dadosdht11.dtCaptura,'%H:%i:%s') as momento_grafico,
+                        DATE_FORMAT(dadoslm35.dtCaptura,'%H:%i:%s'),
+                        sensor.fkPlantacao
+                        from dadosdht11 join sensor on dadosdht11.fkSensor = sensor.idSensor 
+                        join dadoslm35 on dadoslm35.fkSensor = sensor.idSensor where sensor.fkPlantacao = ${idPlantacao} 
+                    order by sensor.fkPlantacao desc limit 1`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
